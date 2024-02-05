64d6e4955b1ceba958859a08b075d209799173f5
balejos2122 <biancaalejos17@gmail.com>
2024-02-04 20:50:29 -0600

    delete function

--- src/App.vue
+++ src/App.vue
@@ -53,6 +53,11 @@
     return Math.floor(Math.random() * 1000000)
   }
 
+  //delete transaction
+  const handleTransactionDeleted = (id) => {
+    transactions.value = transactions.value.filter((transaction) => transaction.id !== id)
+  }
+
 </script>
 
 <template>
